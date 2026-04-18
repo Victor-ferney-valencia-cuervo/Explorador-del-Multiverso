434751b3d6cf7362c92cabeb42025d7c1136adf8
Victor-ferney-valencia-cuervo <pollo123bb@gmail.com>
2026-04-18 23:32:12 +0000

    reparar erroro de carpetas

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ function App(){
       <h1>
         <h1 className="titulo-2">Dimencion Log</h1>
       <p className="titulo-1-inicio">(Explorador del universo)</p>
+      <input className="buscador" type="text"/>
       </h1>
     </div>
   )
